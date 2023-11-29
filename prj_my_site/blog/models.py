05e55722761270254ff3fb272d3ed9de23483517
Suvodip Ghosh <suvodipghosh17@gmail.com>
2023-11-29 21:46:54 +0530

    makemigations migrate createsuperuser

--- prj_my_site/blog/models.py
+++ prj_my_site/blog/models.py
@@ -18,5 +18,5 @@ class Post(models.Model):
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts") # one-to-many
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="posts") # one-to-many
     tags = models.ManyToManyField(Tag)  # many-to-many
