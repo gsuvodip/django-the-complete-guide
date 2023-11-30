cc5cc9e6c3a1a64ae70b48ecdfa947d51e4f256f
Suvodip Ghosh <suvodipghosh17@gmail.com>
2023-11-30 14:58:04 +0530

    Comment model and migrations

--- prj_my_site/blog/models.py
+++ prj_my_site/blog/models.py
@@ -32,3 +32,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Comment(models.Model):
+    user_name = models.CharField(max_length=150)
+    email_address = models.EmailField()
+    text = models.TextField(max_length=400)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
